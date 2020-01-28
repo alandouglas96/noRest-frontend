b39fb87dcc94f3b50c7e7ac55a57d32393f4ad13
alandouglas96 <alandouglas96@gmail.com>
2020-01-28 19:07:17 +0100

    feat: user dashboard implemented, displays all apis that match user

--- client/src/components/presentional/ApiCard.js
+++ client/src/components/presentional/ApiCard.js
@@ -1,14 +1,36 @@
 import React from "react";
 
-const ApiCard = () => {
+const ApiCard = ({apiInfo}) => {
+
+
   return (
     <>
       <h1>API CARD</h1>
       <div style={{}}>
-
+        <div>
+        {apiInfo.public}
+        </div>
+        <div>
+        {apiInfo.api_name}
+        </div>
+        <div>
+        {apiInfo.description}
+        </div>
+        <div>
+        {apiInfo.date_created}
+        </div>
+        <div>
+        {apiInfo.date_last_updated}
+        </div>
       </div>
     </>
   );
 };
 
 export default ApiCard;
+
+
+
+
+
+
